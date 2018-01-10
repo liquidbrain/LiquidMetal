1d4034c99898da39dd716923f2dd295956bda2f3
John Koszarek <john.koszarek@gmail.com>
2018-01-09 20:37:39 -0600

    Moved variables to static constants.

--- LiquidMetal/ShaderBuffers.swift
+++ LiquidMetal/ShaderBuffers.swift
@@ -1,7 +1,4 @@
 //
-//  ShaderBuffers.swift
-//  LiquidMetal
-//
 //  Created by John Koszarek on 9/20/17.
 //  Copyright © 2017 John Koszarek. All rights reserved.
 //
@@ -9,21 +6,35 @@
 import UIKit
 import Metal
 
-class ShaderBuffers {
+/// Functions for creating the buffers used by this app's shaders.
+final class ShaderBuffers {
 
-    static var vertexCount = 0
+    private static let nbrOfPositions = 2   // x and y
 
+    private static let float4x4Size = MemoryLayout<Float>.size * 16                                             // ndcMatrix
+    private static let uniformsStructSize = (MemoryLayout<Float>.size * 16)                                     // ndcMatrix
+                                          + (MemoryLayout<Float>.size * 2)                                      // ptmRatio and pointSize
+                                          + ((MemoryLayout<Float>.size * 4) - (MemoryLayout<Float>.size * 2))   // padding bytes
+
+    static private(set) var vertexCount = 0
+
+    /**
+     Returns a buffer large enough to hold the x and y postions for all particles.
+     */
     class func makeVertexBuffer(device: MTLDevice!, particleSystem: UnsafeMutableRawPointer!) -> MTLBuffer? {
         vertexCount = Int(LiquidFun.particleCount(forSystem: particleSystem))
-        
         let positions = LiquidFun.particlePositions(forSystem: particleSystem)
-        let bufferSize = MemoryLayout<Float>.size * vertexCount * 2
+        let bufferSize = MemoryLayout<Float>.size * vertexCount * nbrOfPositions
         
         let vertexBuffer = device.makeBuffer(bytes: positions!, length: bufferSize, options: [])
         
-        return vertexBuffer;
+        return vertexBuffer
     }
 
+    /**
+     Returns a buffer containing values for our shader uniforms: normalized device coorderinates, points-to-LiquidFun
+     meters ratio, and particle radius to the uniform buffer.
+     */
     class func makeUniformBuffer(device: MTLDevice!, particleRadius: Float, ptmRatio: Float) -> MTLBuffer? {
         // Create the orthographic projection matrix using normalized device coordinates
         // (for near and far).
@@ -31,28 +42,21 @@ class ShaderBuffers {
         let screenWidth = Float(screenSize.width)
         let screenHeight = Float(screenSize.height)
         let ndcMatrix = Math.createOrthographicMatrix(left: 0, right: screenWidth,
-                                                 bottom: 0, top: screenHeight,
-                                                 near: -1, far: 1)
-        
-        // Calculate the size of the Uniforms struct (from Shaders.metal) in memory.
-        let floatSize = MemoryLayout<Float>.size
-        let float4x4ByteAlignment = floatSize * 4
-        let float4x4Size = floatSize * 16   // ndcMatrix
-        let otherFloatsSize = floatSize * 2 // ptmRatio and pointSize
-        let paddingBytesSize = float4x4ByteAlignment - otherFloatsSize
-        let uniformsStructSize = float4x4Size + otherFloatsSize + paddingBytesSize
-        
+                                                      bottom: 0, top: screenHeight,
+                                                      near: -1, far: 1)
+
         // Create the uniform buffer.
         let uniformBuffer = device.makeBuffer(length: uniformsStructSize, options: [])
 
-        // Copy the contents of the Uniform struct data members ??? to the unform buffer.
+        // Copy the normalized device coorderinates, points-to-LiquidFun meters ratio, and particle radius
+        // to the uniform buffer.
         if uniformBuffer != nil {
             let bufferPointer = uniformBuffer!.contents()   // pointer to the shared copy of the buffer data
             var ptmRatioRW = ptmRatio                       // make copy of pmtRatio constant for memcpy
             var particleRadiusRW = particleRadius           // make copy of particleRadius constant for memcpy
             memcpy(bufferPointer, ndcMatrix, float4x4Size)
-            memcpy(bufferPointer + float4x4Size, &ptmRatioRW, floatSize)
-            memcpy(bufferPointer + float4x4Size + floatSize, &particleRadiusRW, floatSize)
+            memcpy(bufferPointer + float4x4Size, &ptmRatioRW, MemoryLayout<Float>.size)
+            memcpy(bufferPointer + float4x4Size + MemoryLayout<Float>.size, &particleRadiusRW, MemoryLayout<Float>.size)
         }
 
         return uniformBuffer;
